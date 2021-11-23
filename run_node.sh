6f43e2a235bfc5d6b78e81fcbe1b56521eba4093
ritnaro yoshikawa <ittanmomennimakaretyaimashita@gmail.com>
2021-11-23 17:25:15 +0900

    docker-compose.yml追加

--- run_node.sh
+++
@@ -1,8 +0,0 @@
-IMAGE_NAME=web_dev_tools
-if docker images | grep -E "^${IMAGE_NAME} "; then
-  echo "\"${IMAGE_NAME}\"イメージは既に存在するのでビルドしません。"
-else
-  echo "\"${IMAGE_NAME}\"イメージがないのでビルドしています..."
-  docker build -q -t $IMAGE_NAME ./
-fi
-docker run --rm -it -v $(pwd):/home/app -w /home/app -p 3000:3000 -p 3001:3001 ${IMAGE_NAME} npm run web-related
